c31a853b6fff88880779620283e083440ce5a994
Elizabeth Afolabi <elizabeth.afolabi@joza-it.fr>
2021-09-21 18:18:57 +0200

    Countries.js update

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -5,13 +5,21 @@ import Card from "./Card";
 
 const Countries = () => {
     const [data, setData] = useState([])
+    const [sortedData, setSortedData] = useState([])
 
     useEffect(() => {
 
         axios.get("https://restcountries.eu/rest/v2/all?fields=name;population;region;capital;flag")
             .then((res) => setData(res.data));
 
-        console.log(data);
+        const sortedCountry = () => {
+            const countryObj = Object.keys(data).map((i) => data[i]);
+            const sortedArray = countryObj.sort((a,b) => {
+                return b.population -a.population;
+            });
+            console.log(sortedArray);
+        };
+        sortedCountry();
     }, []);
 
     return (
